7f9c39889757017a6349bbaa9a0816c3da5b20e0
Tom Casavant <tfcasavant@gmail.com>
2019-04-08 14:04:27 -0400

    Switched from requests to aiohttp (self.http)

--- giphy.py
+++ giphy.py
@@ -1,14 +1,14 @@
 from typing import Type
-from mautrix.types import EventType
-from maubot import Plugin, MessageEvent
-from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
-from maubot.handlers import command
 import requests
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+from maubot import Plugin, MessageEvent
+from maubot.handlers import command
 
 
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("api_key")
+
 
 class GiphyPlugin(Plugin):
     async def start(self) -> None:
@@ -20,16 +20,20 @@ class GiphyPlugin(Plugin):
         return Config
 
     @command.new("giphy")
-    @command.argument("searchTerm", pass_raw=True, required=False)
-    async def handler(self, evt: MessageEvent, searchTerm: str) -> None:
+    @command.argument("search_term", pass_raw=True, required=False)
+    async def handler(self, evt: MessageEvent, search_term: str) -> None:
         await evt.mark_read()
-        if not searchTerm:
-            #If user doesn't supply a search term, set to empty string
-            searchTerm = ""
+        if not search_term:
+            # If user doesn't supply a search term, set to empty string
+            search_term = ""
         api_key = self.config["api_key"]
-        #Get random gif url using search term
-        data = requests.get("http://api.giphy.com/v1/gifs/random?tag={}&api_key={}".format(searchTerm, api_key)).json()
-        gifLink = data['data']['image_url']
+        # Get random gif url using search term
+        async with self.http.get(
+            "http://api.giphy.com/v1/gifs/random?tag={}&api_key={}".format(
+                search_term, api_key
+            )
+        ) as response:
+            data = await response.json()
+        gif_link = data.get("data").get("image_url")
 
-        await evt.reply(gifLink, html_in_markdown=True) #Reply to user
-
+        await evt.reply(gif_link, html_in_markdown=True)  # Reply to user

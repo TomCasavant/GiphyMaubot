8d84e483a92e3cdf644e0b8691b02112bb924afe
Tom Casavant <tfcasavant@gmail.com>
2019-04-08 18:59:57 -0400

    Added ability to choose source of random gifs. Removed error when search returns nothing. Switched to giphy endpoint 'translate'

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A simple [maubot](https://github.com/maubot/maubot) that generates a random gif 
 ## Setup
 1. Get API key from [giphy](https://developers.giphy.com/docs/)
 2. Fill in api_key field in base-config.yaml config file or in online maubot config editor
+3. Decide what endpoint to get random gifs from (e.g. trending, random) in config file
 
 ## Usage
 '!giphy word' - Bot replies with a link to a gif given the search term
--- giphy.py
+++ giphy.py
@@ -1,13 +1,16 @@
 from typing import Type
 import urllib.parse
+import random
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
 
 
+# Setup config file
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
         helper.copy("api_key")
+        helper.copy("source")
 
 
 class GiphyPlugin(Plugin):
@@ -26,13 +29,29 @@ class GiphyPlugin(Plugin):
         if not search_term:
             # If user doesn't supply a search term, set to empty string
             search_term = ""
+            source = self.config["source"]
+        else:
+            source = "translate"
+
         api_key = self.config["api_key"]
-        url_params = urllib.parse.urlencode({"tag": search_term, "api_key": api_key})
+        url_params = urllib.parse.urlencode({"s": search_term, "api_key": api_key})
         # Get random gif url using search term
         async with self.http.get(
-            "http://api.giphy.com/v1/gifs/random?{}".format(url_params)
+            "http://api.giphy.com/v1/gifs/{}?{}".format(source, url_params)
         ) as response:
             data = await response.json()
-        gif_link = data.get("data", {}).get("image_url")
 
-        await evt.reply(gif_link, html_in_markdown=True)  # Reply to user
+        # Retrieve gif link from JSON response
+        gif = data.get("data", {})
+        gif_exists = True
+        if isinstance(gif, list):
+            # check if there were no results
+            if gif:
+                gif_link = random.choice(gif).get("url")
+            else:
+                gif_exists = False
+        else:
+            gif_link = gif.get("url")
+
+        if gif_exists:
+            await evt.reply(gif_link, html_in_markdown=True)  # Reply to user
